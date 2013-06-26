d9b4fde9d5d529405bbad63b06c021be977ee3d6
Ryan Tablada <ryan.tablada@gmail.com>
2013-06-26 12:56:11 -0500

    All done

---
+++ src/Rtablada/WardrobeDisqus/WardrobeDisqus.php
@@ -0,0 +1,30 @@
+<?php namespace Rtablada\WardrobeDisqus;
+
+use Illuminate\Config\Repository as Config;
+
+class WardrobeDisqus
+{
+	protected $config;
+
+	public function __construct(Config $config)
+	{
+		$this->config = $config;
+	}
+
+	public function comments()
+	{
+		$disqus_shortname = $this->config->get('rtablada::disqus_shortname');
+
+		return
+			"<div id=\"disqus_thread\"></div>\n" .
+			"<script type=\"text/javascript\">\n\t" .
+			"var disqus_shortname = '{$disqus_shortname	}';\n\t" .
+			"(function() {\n\t\t" .
+			"var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;\n\t\t" .
+			"dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';\n\t\t" .
+			"(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);\n\t" .
+			"})();\n</script>\n" .
+			"<noscript>Please enable JavaScript to view the <a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>\n" .
+			"<a href=\"http://disqus.com\" class=\"dsq-brlink\">comments powered by <span class=\"logo-disqus\">Disqus</span></a>\n";
+	}
+}
---
+++ tests/Rtablada/WardrobeDisqus/WardrobeDisqusTest.php
@@ -0,0 +1,28 @@
+<?php namespace Rtablada\WardrobeDisqus;
+
+use Mockery as m;
+
+class WardrobeDisqusTest extends \PHPUnit_Framework_Testcase
+{
+	public function setup()
+	{
+		$this->resultComment = file_get_contents(__DIR__ . '/stubs/disqus.txt');
+
+		$config = m::mock('Illuminate\Config\Repository');
+		$config->shouldReceive('get')->once()->with('rtablada::disqus_shortname')->andReturn('example');
+
+		$this->disqus = new WardrobeDisqus($config);
+	}
+
+	public function teardown()
+	{
+		m::close();
+	}
+
+	public function testCanCreateComments()
+	{
+		$comments = $this->disqus->comments();
+
+		$this->assertEquals($this->resultComment, $comments);
+	}
+}
